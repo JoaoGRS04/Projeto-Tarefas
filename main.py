6eb01607284b1a2e3af5a1c3a7764c7ca2f706cf
JoaoGRS04 <joaoshowgabriel@gmail.com>
2026-05-29 19:13:02 -0300

    feat: add json function

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+
+def load_tasks():
+    try:
+        with open('tasks.json', 'r') as arquivo:
+            return json.load(arquivo)
+    except:
+        return []
+
+
+def save_tasks(tarefas):
+    with open('tasks.json', 'w') as arquivo:
+        json.dump(tarefas, arquivo, indent=4)
